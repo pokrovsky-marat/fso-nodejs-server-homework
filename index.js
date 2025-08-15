52433ff8b95c064c7d6b2f2ea2a82d477e975f98
Your Name <you@example.com>
2025-08-15 14:53:13 +0600

    3.21 version

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const Person = require("./models/person");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
@@ -32,9 +34,11 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   let body = req.body;
   if (!body?.name || !body?.number) {
     res.status(400);
@@ -42,42 +46,93 @@ app.post("/api/persons", (req, res) => {
   }
 
   if (body?.number && body?.name) {
-    if (persons.find((person) => person.name == body?.name)) {
-      res.status(400);
-      res.json({ error: "name must be unique" });
-    } else {
-      let person = { ...body, id: String(Math.random()) };
-      persons.push(person);
-      res.json(person);
-    }
+    // if (persons.find((person) => person.name == body?.name)) {
+    //   res.status(400);
+    //   res.json({ error: "name must be unique" });
+    // } else {
+    //   let person = { ...body, id: String(Math.random()) };
+    //   persons.push(person);
+    //   res.json(person);
+    // }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    });
+
+    person
+      .save()
+      .then((savedPerson) => {
+        res.json(savedPerson);
+      })
+      .catch((error) => {
+        next(error);
+      });
   }
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
-app.get("/api/persons/:id", (req, res) => {
-  let id = req.params.id;
-  let person = persons.find((person) => person.id == id);
-  if (!person) {
-    res.status(404);
-    res.end();
-  } else {
-    res.json(person);
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 app.get("/info", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  res.write(`Phonebook has info for ${persons.length} people`);
-  res.write("<br/>");
-  let date = new Date();
-  res.write(String(date));
-  res.end();
+  Person.find({}).then((persons) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write(`Phonebook has info for ${persons.length} people`);
+    res.write("<br/>");
+    let date = new Date();
+    res.write(String(date));
+    res.end();
+  });
 });
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const { name, number } = req.body;
+
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).end();
+      }
+      person.name = name;
+      person.number = number;
+
+      return person.save().then((updatedPerson) => {
+        res.json(updatedPerson);
+      });
+    })
+    .catch((error) => next(error));
+});
+const errorHandler = (error, req, res, next) => {
+  console.error(error);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 // Это настройка для деплоя на render.com
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is runnin on ${PORT}`);
 });
